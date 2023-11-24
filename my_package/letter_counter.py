bbf399b238c4b5fa840aa691af28187a4bc17021
rapampamfau <rafal.szczepanczyk7@gmail.com>
2023-11-24 22:22:40 +0100

    feat: added sqlite database for post storage

--- my_package/letter_counter.py
+++ my_package/letter_counter.py
@@ -1,35 +1,68 @@
 import requests
-
-# Downloading posts from site
-url = 'https://jsonplaceholder.typicode.com/posts'
-response = requests.get(url)
-posts = response.json()
-
-# Dict init for letter count
-letter_count = {}
-for letter in 'abcdefghijklmnopqrstuvwxyz':
-    letter_count[letter] = 0
-
-# Processing every post
-for post in posts:
-    if 'body' in post:
-        body = post['body'].lower()  # Changing letters to lower for cohesion
-        for char in body:
-            if char.isalpha():
-                letter_count[char] += 1
-
-# Printing the output in console
-for letter, count in letter_count.items():
-    print(f"{letter}: {count}")
-
-# Graphical oputput
+import sqlite3
 import matplotlib.pyplot as plt
 
-letters = letter_count.keys()
-counts = letter_count.values()
+# Download 1000 posts
+url = 'https://jsonplaceholder.typicode.com/posts'
+response = requests.get(url)
+posts = response.json()[:1000]
+
+# Count letters in the 'body' field
+letter_counts = {}
+for post in posts:
+    body = post['body']
+    for letter in body:
+        if letter.isalpha():
+            letter = letter.lower()
+            letter_counts[letter] = letter_counts.get(letter, 0) + 1
+
+# Generate a frequency histogram
+letters = list(letter_counts.keys())
+counts = list(letter_counts.values())
 
 plt.bar(letters, counts)
-plt.xlabel('Letter')
-plt.ylabel('Number of occurrences')
-plt.title('The number of occurrences of letters in the body of post')
+plt.xlabel('Letters')
+plt.ylabel('Frequency')
+plt.title('Letter Frequency in Posts Body')
 plt.show()
+
+# Connect to SQLite database (creates a new one if not exists)
+conn = sqlite3.connect('example.db')
+
+# Create a cursor object to execute SQL queries
+cursor = conn.cursor()
+
+# Clear the 'posts' table
+cursor.execute('DELETE FROM posts')
+
+# Create a table to store post data
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS posts (
+        id INTEGER PRIMARY KEY,
+        userId INTEGER,
+        title TEXT,
+        body TEXT
+    )
+''')
+
+# Insert posts into the table
+for post in posts:
+    cursor.execute('''
+        INSERT INTO posts (userId, title, body)
+        VALUES (?, ?, ?)
+    ''', (post['userId'], post['title'], post['body']))
+
+# Commit the changes
+conn.commit()
+
+# Validate if the number of rows matches the expected number of posts
+cursor.execute('SELECT * FROM posts')
+rows = cursor.fetchall()
+expected_num_posts = len(posts)
+if len(rows) == expected_num_posts:
+    print(f"Successfully inserted {expected_num_posts} posts into the database.")
+else:
+    print(f"Error: Expected {expected_num_posts} posts, but found {len(rows)} posts in the database.")
+
+# Close the connection
+conn.close()
